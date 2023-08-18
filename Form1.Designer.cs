165e23729f25918b089f2fd9519248dc9c3989b9
Ruslan <abishev2012@gmail.com>
2023-08-18 23:21:57 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PCinfoServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.nudPort = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPort)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(85, 161);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Старт";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // nudPort
+            // 
+            this.nudPort.Location = new System.Drawing.Point(62, 114);
+            this.nudPort.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.nudPort.Name = "nudPort";
+            this.nudPort.Size = new System.Drawing.Size(120, 20);
+            this.nudPort.TabIndex = 1;
+            this.nudPort.Value = new decimal(new int[] {
+            1234,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите номер порта";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 230);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudPort);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudPort)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.NumericUpDown nudPort;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,183 @@
+﻿using MongoDB.Driver.Core.Configuration;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace PCinfoServer
+{
+    public partial class Form1 : Form
+    {
+        public class ServerSettings
+        {
+            public int Port { get; set; }
+        }
+        private TcpListener tcpListener;
+        public Form1()
+        {
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Загрузка настроек сервера из файла
+            _ = LoadSettings();
+        }
+
+       
+
+        private ServerSettings LoadSettings()
+        {
+            try
+            {
+                string settingsFile = "server_settings.txt";
+
+                if (File.Exists(settingsFile))
+                {
+                    using (StreamReader sr = new StreamReader(settingsFile))
+                    {
+                        string json = sr.ReadToEnd();
+                        return Newtonsoft.Json.JsonConvert.DeserializeObject<ServerSettings>(json);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка загрузки настроек сервера: " + ex.Message);
+            }
+
+            return null;
+        }
+
+        private void SaveSettings(ServerSettings settings)
+        {
+            try
+            {
+                string settingsFile = "server_settings.txt";
+                string json = Newtonsoft.Json.JsonConvert.SerializeObject(settings);
+
+                using (StreamWriter sw = new StreamWriter(settingsFile))
+                {
+                    sw.Write(json);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка сохранения настроек сервера: " + ex.Message);
+            }
+        }
+
+        private void StartServer(int port)
+        {
+
+            try
+            {
+                // Создание TCP прослушивателя
+                tcpListener = new TcpListener(IPAddress.Any, port);
+                tcpListener.Start();
+
+                // Ожидание подключений
+                while (true)
+                {
+                    // Принятие подключения
+                    TcpClient client = tcpListener.AcceptTcpClient();
+
+                    // Обработка подключения в отдельном потоке
+                    HandleClient(client);
+                }
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show($"Ошибка запуска сервера: {ex.Message}");
+            }
+            finally
+            {
+                if (tcpListener != null)
+                {
+                    tcpListener.Stop();
+                }
+            }
+        }
+
+        private void HandleClient(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream);
+
+            string request = reader.ReadLine();
+            string response = ProcessRequest(request);
+
+            writer.WriteLine(response);
+            writer.Flush();
+
+            client.Close();
+        }
+
+        private string ProcessRequest(string request)
+        {
+            if (request == "get_info")
+            {
+                return "Введите тип запроса: user_info (получить информацию о пользователе) или disk_info (получить информацию о диске).";
+            }
+            else if (request == "user_info")
+            {
+                string userName = Environment.UserName;
+                string myDocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                return $"Имя пользователя: {userName}\n" +
+                       $"Путь к 'Мои документы': {myDocumentsPath}";
+            }
+            else if (request == "disk_info")
+            {
+                string driveName = Directory.GetDirectoryRoot(Environment.SystemDirectory);
+                DriveInfo drive = new DriveInfo(driveName);
+
+                return $"Диск: {driveName}\n" +
+                       $"Свободное место на диске: {drive.AvailableFreeSpace / (1024 * 1024)} MB";
+            }
+
+            return "Неверный запрос.";
+        }
+    
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Остановка сервера при закрытии формы
+            if (tcpListener != null)
+            {
+                tcpListener.Stop();
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (tcpListener != null && tcpListener.Server.IsBound)
+            {
+                MessageBox.Show("Сервер уже запущен.");
+                return;
+            }
+
+            int port = (int)nudPort.Value;
+
+            if (port < 1 || port > 65535)
+            {
+                MessageBox.Show("Недопустимый номер порта. Введите значение от 1 до 65535.");
+                return;
+            }
+
+            // Сохранение настроек сервера
+            ServerSettings settings = new ServerSettings { Port = port };
+            SaveSettings(settings);
+
+            // Запуск сервера
+            StartServer(port);
+
+            MessageBox.Show("Сервер запущен!");
+        }
+    }
+}
